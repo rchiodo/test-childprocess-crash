f54b99726f2478db47e163e4256e714281b753d9
rchiodo <rchiodo@microsoft.com>
2023-10-19 20:16:31 -0700

    Update readme

--- scripts/get_pytest_options.py
+++ scripts/get_pytest_options.py
@@ -1,7 +1,9 @@
 import sys
 from _pytest.config import get_config
+import time
 
 c = get_config()
+time.sleep(30)
 c.parse(sys.argv)
 print(f"Python classes: {c.getini('python_classes')}")
 print(f"Python files: {c.getini('python_files')}")
