618c9c13e3ba7ce094590c63427310f1b2740ce6
rchiodo <rchiodo@microsoft.com>
2023-10-19 22:36:13 -0700

    Try using spawn instead

--- src/child_launcher.ts
+++ src/child_launcher.ts
@@ -1,16 +1,53 @@
 // Create a python process that just waits
-import { exec } from 'child_process';
-import * as path from 'path';
+import { spawn } from "child_process";
+import * as path from "path";
+import { CancellationToken, CancellationTokenSource } from "vscode-jsonrpc";
 
-const scriptPath = path.join(process.argv[1], '..', '..', 'scripts', 'get_pytest_options.py')
+const scriptPath = path.join(
+  process.argv[2],
+  "..",
+  "scripts",
+  "get_pytest_options.py"
+);
+function spawnProcess(token: CancellationToken) {
+  // What to do about conda here?
+  return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
+    let stdout = "";
+    let stderr = "";
+    const args = [scriptPath];
+
+    const child = spawn("python", args, { cwd: process.argv[2] });
+    const tokenWatch = token.onCancellationRequested(() => {
+      child!.kill();
+      reject(new Error("Canceled"));
+    });
+    if (child) {
+      child.stdout.on("data", (d) => (stdout = stdout.concat(d)));
+      child.stderr.on("data", (d) => (stderr = stderr.concat(d)));
+      child.on("error", (e) => {
+        tokenWatch.dispose();
+        reject(e);
+      });
+      child.on("exit", () => {
+        tokenWatch.dispose();
+        resolve({ stdout, stderr });
+      });
+    } else {
+      tokenWatch.dispose();
+      reject(
+        new Error(`Cannot start python interpreter with script ${scriptPath}`)
+      );
+    }
+  });
+}
 console.log(`Starting python process.`);
-const child = exec(`python ${scriptPath}`);
+const tokenSource = new CancellationTokenSource();
+const spawnPromise = spawnProcess(tokenSource.token);
 setTimeout(() => {
-    console.log(`Trying to kill python process`);
-    // Kill it after a second
-    child.kill();
-    console.log(`Killed python process`);
-}, 100);
-child.on('error', (e) => console.error(e));
-child.stdout?.on('data', (d) => console.log(d));
-child.stderr?.on('data', (d) => console.error(d));
+    tokenSource.cancel();
+}, 1000);
+spawnPromise.then(v => {
+    console.log(`Spawned promise finished.`);
+    console.log(v.stdout);
+    console.error(v.stderr);
+}); // Don't catch?
